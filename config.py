e52d038e3093e3841bed058aa5465723f74177f4
vahidkhan200 <vahidelder200@gmail.com>
2025-04-17 01:06:56 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 # config.py
+import os
 
-# توکن و چت آیدی تلگرام (مقدارها رو با مقادیر واقعی جایگزین کن)
-TELEGRAM_BOT_TOKEN = '7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4'
-TELEGRAM_CHAT_ID = '392018191'
+TELEGRAM_BOT_TOKEN = os.getenv("7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4")
+TELEGRAM_CHAT_ID = os.getenv("392018191")
 
-# لیست ارزهای موردنظر (مطابق با نمادهای LBank)
-SYMBOLS = [symbols = [
-    "btc_usdt", "eth_usdt", "bnb_usdt", "sol_usdt", "ada_usdt", 
-    "xrp_usdt", "dot_usdt", "matic_usdt", "ltc_usdt", "grt_usdt",
-    "grs_usdt", "not_usdt", "doge_usdt", "xlm_usdt", "hbar_usdt", "xdc_usdt"
+SYMBOLS = [
+    "btc_usdt", "eth_usdt", "bnb_usdt", "sol_usdt", "ada_usdt", "xrp_usdt",
+    "dot_usdt", "matic_usdt", "ltc_usdt", "grt_usdt", "grs_usdt",
+    "not_usdt", "doge_usdt", "xlm_usdt", "hbar_usdt", "xdc_usdt"
 ]
 
 # تایم‌فریم تحلیل
