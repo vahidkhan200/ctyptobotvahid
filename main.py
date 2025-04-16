8f65c981df6e5f8d32f5ff354daf1bb6c0d31ec4
vahidkhan200 <vahidelder200@gmail.com>
2025-04-16 17:03:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from lbank_api import get_ohlcv
+from config import SYMBOLS, INTERVAL
 from telegram_bot import send_telegram_message
 
+def main():
+    for symbol in SYMBOLS:
+        try:
+            data = get_ohlcv(symbol, INTERVAL)
+            if data:
+                send_telegram_message(f"دیتای {symbol} با موفقیت دریافت شد.")
+            else:
+                send_telegram_message(f"دیتایی برای {symbol} پیدا نشد.")
+        except Exception as e:
+            send_telegram_message(f"خطا در دریافت دیتا برای {symbol}: {str(e)}")
+
 if __name__ == "__main__":
-    message = "سلام! این یک پیام تستی از ربات تلگرامه."
-    send_telegram_message(message)
+    main()
