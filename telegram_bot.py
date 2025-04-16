3ca158482cdad5e13db1ba4f1acfb5c0f3d100e1
vahidkhan200 <vahidelder200@gmail.com>
2025-04-16 17:11:53 +0330

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,5 @@
 import requests
-from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
