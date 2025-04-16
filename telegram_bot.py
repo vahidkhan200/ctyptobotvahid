fed41ac9e5e1f51a1e621fcc429f25128b4b74af
vahidkhan200 <vahidelder200@gmail.com>
2025-04-16 16:56:35 +0330

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,11 @@
 import requests
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-def send_telegram_message(message: str):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message
     }
-    response = requests.post(url, data=payload)
-    return response.json()
+    response = requests.post(url, data=data)
+    print(response.json())
