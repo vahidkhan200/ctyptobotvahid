83fc2cc4ad04d2aaa61e6ab64c3b9fede6071d0f
vahidkhan200 <vahidelder200@gmail.com>
2025-04-16 16:54:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import test_telegram
-git add .
-git commit -m "Run telegram test"
-git push
+from telegram_bot import send_telegram_message
+
+if __name__ == "__main__":
+    message = "سلام! این یک پیام تستی از ربات تلگرامه."
+    send_telegram_message(message)
