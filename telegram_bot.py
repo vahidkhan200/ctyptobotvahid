291316fff7996dd30ade16d00cb7a4f8ec3925e3
vahidkhan200 <vahidelder200@gmail.com>
2025-04-17 23:18:34 +0330

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,14 +1,10 @@
 import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
-def send_telegram_message(text):
+def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    data = {"chat_id": TELEGRAM_CHAT_ID, "text": text}
-    requests.post(url, data=data)
-
-def send_telegram_photo(photo_path, caption=""):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPhoto"
-    with open(photo_path, 'rb') as photo:
-        files = {'photo': photo}
-        data = {"chat_id": TELEGRAM_CHAT_ID, "caption": caption}
-        requests.post(url, files=files, data=data)
+    data = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
+    try:
+        requests.post(url, data=data)
+    except Exception as e:
+        print(f"خطا در ارسال پیام تلگرام: {str(e)}")
