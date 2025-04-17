deffcd5cbab47a50093173cbbabe5e1224f13e9b
vahidkhan200 <vahidelder200@gmail.com>
2025-04-17 22:00:48 +0330

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,20 +1,14 @@
 import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
-def send_telegram_message(message):
+def send_telegram_message(text):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML"
-    }
-    requests.post(url, data=payload)
+    data = {"chat_id": TELEGRAM_CHAT_ID, "text": text}
+    requests.post(url, data=data)
 
-def send_telegram_photo(photo_path, caption=None):
+def send_telegram_photo(photo_path, caption=""):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPhoto"
     with open(photo_path, 'rb') as photo:
         files = {'photo': photo}
-        data = {'chat_id': TELEGRAM_CHAT_ID}
-        if caption:
-            data['caption'] = caption
+        data = {"chat_id": TELEGRAM_CHAT_ID, "caption": caption}
         requests.post(url, files=files, data=data)
