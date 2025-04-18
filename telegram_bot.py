d070ccda262e38e4c60eda72865b4cfa489d602d
vahidkhan200 <vahidelder200@gmail.com>
2025-04-18 10:54:35 +0330

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,7 +4,4 @@ from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
-    try:
-        requests.post(url, data=data)
-    except Exception as e:
-        print(f"خطا در ارسال پیام تلگرام: {str(e)}")
+    requests.post(url, data=data)
