e8115d689525ebb0bd0ac9347f8c46703783b365
vahidkhan200 <vahidelder200@gmail.com>
2025-04-19 09:23:46 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ CHECK_INTERVAL = 600  # 10 دقیقه
 # اگر از متغیر محیطی استفاده نمی‌کنی، توکن رو مستقیم بنویس
 TELEGRAM_BOT_TOKEN = "7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4"
 TELEGRAM_CHAT_ID = "392018191"
+LBANK_API_URL = "https://api.lbank.info"
