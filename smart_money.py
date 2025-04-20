5fffbec09b073ba94be558283821b747ce6077c7
vahidkhan200 <vahidelder200@gmail.com>
2025-04-20 20:54:55 +0330

    Create smart_money.py

---
+++ smart_money.py
@@ -0,0 +1,9 @@
+# تشخیص S&D Zones
+def supply_and_demand_zones(candles):
+    # شناسایی مناطق عرضه و تقاضا
+    pass
+
+# تشخیص لیکوییدیتی و سفارش‌ها
+def liquidity_sweeps(candles):
+    # شناسایی لیکوییدیتی
+    pass
