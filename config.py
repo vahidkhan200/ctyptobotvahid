b7d13162e7d85dc7c0e18e00257b9d82b0c1d955
vahidkhan200 <vahidelder200@gmail.com>
2025-04-20 20:43:22 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
-SYMBOLS = ["BTC/USDT", "ETH/USDT", "XRP/USDT", "DOGE/USDT", "HBAR/USDT", "XDC/USDT", "GRT/USDT", "NOT/USDT"]
-TIMEFRAMES = ["1h", "4h"]
-CHECK_INTERVAL = 600  # 10 دقیقه
+import os
 
-# اگر از متغیر محیطی استفاده نمی‌کنی، توکن رو مستقیم بنویس
-TELEGRAM_BOT_TOKEN = "7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4"
-TELEGRAM_CHAT_ID = "392018191"
-LBANK_API_URL = "https://api.lbank.info"
+# API اطلاعات صرافی
+BINANCE_API_KEY = '6bf9774e-2f4d-4434-ad0a-e54cf1ba2ca5'
+BINANCE_API_SECRET = '31B770319F1E34E686393D3486D38C01'
+
+# توکن ربات تلگرام
+TELEGRAM_BOT_TOKEN = '7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4'
+TELEGRAM_CHAT_ID = '392018191'  # یا @your_username
+
+# تایم‌فریم پیش‌فرض برای تحلیل
+DEFAULT_TIMEFRAME = '5m'
