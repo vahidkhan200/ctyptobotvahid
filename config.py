10a8862094bda3139efcd6c745e4d27e970f927d
vahidkhan200 <vahidelder200@gmail.com>
2025-04-20 21:20:17 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-import os
+# config.py
 
-# API اطلاعات صرافی
-BINANCE_API_KEY = '6bf9774e-2f4d-4434-ad0a-e54cf1ba2ca5'
-BINANCE_API_SECRET = '31B770319F1E34E686393D3486D38C01'
+# اطلاعات LBank
+LBANK_API_KEY = '6bf9774e-2f4d-4434-ad0a-e54cf1ba2ca5'
+LBANK_API_SECRET = '31B770319F1E34E686393D3486D38C01'
+LBANK_BASE_URL = 'https://api.lbkex.com'
 
-# توکن ربات تلگرام
+# اطلاعات تلگرام
 TELEGRAM_BOT_TOKEN = '7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4'
-TELEGRAM_CHAT_ID = '392018191'  # یا @your_username
+TELEGRAM_CHAT_ID = '392018191'
 
-# تایم‌فریم پیش‌فرض برای تحلیل
-DEFAULT_TIMEFRAME = '5m'
+# تایم‌فریم پیش‌فرض
+DEFAULT_TIMEFRAME = '5min'
