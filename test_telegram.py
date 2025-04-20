4adceeb0f2fdb133ee656907163a136db6b7c54e
vahidkhan200 <vahidelder200@gmail.com>
2025-04-20 22:16:44 +0330

    Update test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -4,9 +4,9 @@ import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 def send_test_message():
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{7769467888:AAHrxpGS8xdM8EzJHz5qVjS774arSP1VfLU}/sendMessage"
     data = {
-        "chat_id": TELEGRAM_CHAT_ID,
+        "chat_id": 392018191,
         "text": "سلام! این یک پیام تستی از ربات تریدره."
     }
     response = requests.post(url, data=data)
