f06ce289322987e61475d2afa3d43878f7683178
vahidkhan200 <vahidelder200@gmail.com>
2025-04-20 21:28:03 +0330

    Update test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -1,1 +1,18 @@
+# test_telegram.py
 
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_test_message():
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": "سلام! این یک پیام تستی از ربات تریدره."
+    }
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        print("پیام با موفقیت ارسال شد.")
+    else:
+        print("خطا در ارسال پیام:", response.text)
+
+send_test_message()
