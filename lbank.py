6d198f0490e2dbf37cc76184118f3bb925e0b481
vahidkhan200 <vahidelder200@gmail.com>
2025-04-20 20:49:22 +0330

    Update lbank.py

--- lbank.py
+++ lbank.py
@@ -1,1 +1,17 @@
+import ccxt
+from config import LBANK_API_KEY, LBANK_API_SECRET
 
+# اتصال به Lbank API
+def create_lbank_client():
+    lbank = ccxt.lbank({
+        'apiKey': LBANK_API_KEY,
+        'secret': LBANK_API_SECRET,
+        'enableRateLimit': True,
+    })
+    return lbank
+
+# دریافت داده‌های کندلی
+def get_candles(symbol, timeframe='5m', limit=100):
+    lbank = create_lbank_client()
+    candles = lbank.fetch_ohlcv(symbol, timeframe, limit=limit)
+    return candles
