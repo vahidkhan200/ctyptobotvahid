4d1bb7fb049a3886db1edb85a1309e29cdc12df2
vahidkhan200 <vahidelder200@gmail.com>
2025-04-20 20:52:02 +0330

    Update chart_patterns.py

--- chart_patterns.py
+++ chart_patterns.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
 
+# شناسایی سقف و کف دوقلو
+def double_top_bottom(candles):
+    # تحلیل داده‌های کندلی و شناسایی الگو
+    pass
+
+# شناسایی سر و شانه
+def head_and_shoulders(candles):
+    # تحلیل داده‌ها و شناسایی الگو
+    pass
+
+# سایر الگوها هم همینطور به همین شکل پیاده‌سازی می‌شن
